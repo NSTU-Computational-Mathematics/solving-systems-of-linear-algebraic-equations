3b65c51d88d05dd55e87d1bc6c2275cb761b193e
xpomin <xpomin@pop-os.localdomain>
2023-03-15 10:22:51 +0700

    refactor

--- task_2.py
+++ task_2.py
@@ -2,35 +2,37 @@ import numpy as np
 
 
 def check_convergence(A):
-    size = len(A)
-
-    for row in range(size):
-        if A[row][row] <= sum(A[row]) - A[row][row]:
+    for row in range(len(A)):
+        if 2 * A[row][row] <= sum(A[row]):
             return False
 
     return True
 
+def is_converged(diff, A_norm, eps):
+    return np.linalg.norm(diff) <= eps \
+                or A_norm > 1 / 2 and np.linalg.norm(diff) <= ((1 - A_norm) / A_norm) * eps
 
 def solve_system(alpha, beta, eps):
     """
     Решает систему уравнений с помощью простого итерационного метода.
 
     Parameters:
-        alpha (numpy.ndarray): Двумерный массив значений.
-        beta (numpy.ndarray): Вертикальная решетка.
+        alpha (numpy.ndarray): Двумерный массив коэффициентов при неизвестных.
+        beta (numpy.ndarray): Правая часть СЛАУ.
         eps (float): Критерий остановки.
 
     Returns:
-        x (numpy.ndarray): Решение системы уравнений.
+        x (numpy.ndarray): Вектор решения системы уравнений.
     """
-    if not check_convergence(alpha):
-        return None
+    # if not check_convergence(alpha):
+    #     return None
 
     size = len(alpha)
     solution = [0] * size
     A_norm = np.linalg.norm(alpha)
 
     while True:
+
         next_step = [0] * size
 
         for row in range(size):
@@ -46,14 +48,19 @@ def solve_system(alpha, beta, eps):
         for i in range(size):
             diff[i] = solution[i] - next_step[i]
 
+        
         solution = next_step
 
-        if np.linalg.norm(diff) <= eps \
-                or A_norm > 1 / 2 and np.linalg.norm(diff) <= ((1 - A_norm) / A_norm) * eps:
+        if is_converged(diff, A_norm, eps):
             return solution
 
 
-alpha = np.array([[20, 2, 3, 7], [1, 12, -2, -5], [5, -3, 13, 0], [0, 0, -1, 15]])
+alpha = np.array([
+    [20, 2, 3, 7], 
+    [1, 12, -2, -5],
+    [5, -3, 13, 0], 
+    [0, 0, -1, 15]
+    ])
 beta = np.array([5, 4, -3, 7])
 x = solve_system(alpha, beta, 1e-3)
 print(x)
