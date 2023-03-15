e33db3bff08b79120236d0eadcc3035d052a42a7
xpomin <xpomin@pop-os.localdomain>
2023-03-15 11:27:22 +0700

    set-up for task 2

---
+++ task_3.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def solve_system(alpha, beta):
+    size = len(alpha)
+    solution = [0] * size
+
+    for i in range(1, size):
+        kf = alpha[i][i-1] / alpha[i-1][i-1]
+        alpha[i][i] -= kf * alpha[i-1][i]
+        beta[i] -= kf * beta[i-1]
+        alpha[i][i - 1] = 0
+    print(alpha)
+    print(beta)
+
+    solution[size-1] = beta[size-1] / alpha[size-1][size-1]
+    print(solution)
+    for i in range(size-2, 1, -1):
+        solution[i] = beta[i] / (solution[i+1] * alpha[i][i+1])
+
+    print(solution)
+    return solution
+
+
+alpha = np.array([
+    [8.0, -2, 0, 0, 0],
+    [-1, 5, 3, 0, 0],
+    [0, 7, -5, 9, 0],
+    [0, 0, 4, 7, 9],
+    [0, 0, 0, -5, 8]
+])
+beta = np.array([-7.0, 6, 9, -8, 5])
+x = solve_system(alpha, beta)
+# print(x)
